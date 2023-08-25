1770e261ab10e6b8cc605af85ee26f371a796f43
Lucas <90220811+lucasasr25@users.noreply.github.com>
2023-08-24 22:00:12 -0300

    Delete public_html directory

--- public_html/PHP/private/config.php
+++
@@ -1,7 +0,0 @@
-<?php
- // Define database connection constants
-    define('HOST','localhost');
-    define('DATABASE', 'id20689628_products');
-    define('USER', 'id20689628_root');
-    define('PASSWORD','=K&4xJ#xSB$!ZAO8');
-?>
